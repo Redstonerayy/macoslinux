1ae5d55ab154d87b2e2ba54e8b1d68dff767fd26
Redstonerayy <antonpaulr@protonmail.com>
2022-06-13 22:53:17 +0200

    chanes

--- setup.sh
+++ setup.sh
@@ -4,7 +4,14 @@ mkdir $HOME/.themes
 mkdir $HOME/.icons
 
 #copy files
+cp -r whitesur-dark-theme $HOME/.themes
+cp -r whitesur-light-theme $HOME/.themes
+cp -r mojave-cursors $HOME/.icons
+cp -r whitesur-grey-icons $HOME/.icons
+cp -r whitesur-purple-icons $HOME/.icons
+
 
 #copy settings files
 
 #reboot
+#sudo reboot now
