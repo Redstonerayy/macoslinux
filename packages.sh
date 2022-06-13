083a6cdb116d66b140cb3f6c014759e3a5af241f
Redstonerayy <antonpaulr@protonmail.com>
2022-06-14 00:52:27 +0200

    lightdm not working. goal is to make it working without a look screen

--- packages.sh
+++ packages.sh
@@ -2,5 +2,6 @@
 yay -Syu vala-panel vala-panel-appmenu-xfce-git \
 vala-panel-appmenu-registrar-git panther-launcher-git \
 synapse appmenu-gtk-module lightdm-webkit2-greeter \
-lightdm-webkit2-them-glorious
+lightdm-webkit2-theme-glorious \
+python-whither lightdm python-gobject
 sudo pacman -Syu plank
